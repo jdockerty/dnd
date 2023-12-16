27e9d829b03e1eeb27ab116d1f4a09fd5bc55bab
Jack Dockerty <jdockerty@influxdata.com>
2023-12-16 16:05:47 +0000

    wip gossip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tokio::net::{UdpSocket, ToSocketAddrs};
+use clap::Parser;
+
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Server {
+
+    #[arg(short, long)]
+    seed_peers: Option<Vec<String>>
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("Starting!");
+    let server = Server::parse();
+        let socket = UdpSocket::bind("127.0.0.1:5000").await?;
+        println!("Bound to {}", socket.local_addr()?);
+
+    // Idea: on startup take a vec of seed peers. Connect to these and send/recv
+    // from them.
+    // How to handle starting from 0? Receive from bound socket and other peers send their known
+    // peers.
+    tokio::task::spawn(async move {
+
+        if let Some(seed_peers) = server.seed_peers {
+            for peer in seed_peers {
+                socket.connect(peer).await.unwrap();
+
+            }
+
+        }
+    })
+    .await;
+
+    match tokio::signal::ctrl_c().await {
+        Ok(_) => println!("Cancelled"),
+        Err(e) => println!("error on cancel: {e}"),
+    }
+
+    Ok(())
 }
